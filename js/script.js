93dddd077016cba965f4d8e5a41968f85df8da8e
JorgeLuiz <jrgluiz4848@gmail.com>
2023-01-29 22:54:50 -0300

    adicionado sistema para mudar a cor do tema

--- js/script.js
+++ js/script.js
@@ -76,11 +76,11 @@ function carregaNotasSalvas(){
     let cores = '';
     let btnTema = document.querySelector('.theme');
     let espacoNota = document.querySelector('.principal');
-    
+
     if(window.localStorage.key('titulo')){
-        titulos = window.localStorage.getItem('titulo').split(',') || '';
-        descricoes = window.localStorage.getItem('descricao').split(',') || '';
-        cores = window.localStorage.getItem('cor').split(',') || '';
+        titulos = window.localStorage.getItem('titulo').split(',');
+        descricoes = window.localStorage.getItem('descricao').split(',');
+        cores = window.localStorage.getItem('cor').split(',');
         for(let i = 0; i < titulos.length; i++){
             let nota = `<div class="note"><div class="head"><p>${titulos[i]}</p></div><div class="body"><div class="body-conteudo"><div class="nota-info"> <div class="ck-finalizado"><input type="checkbox" name="finalizado" id="finalizado"><label for="finalizado">Finalizado</label></div><div class="nota-data"><span>00/00/0000</span></div></div><p>${descricoes[i]}</p></div></div></div>`;
             espacoNota.innerHTML += nota;
@@ -127,6 +127,74 @@ btnTema.addEventListener('click', () => {
     
 })
 
-function mudaCor(){
+let btnCor = document.querySelector('.change');
+btnCor.addEventListener('click', mudaCor);
+let clicouBtnCor = false;
 
+function mudaCor(){
+    clicouBtnCor = !clicouBtnCor;
+    let barraCor = document.querySelector('.barra-lateral-suspensa');
+
+    if(clicouBtnCor){
+        barraCor.style.display = 'flex';
+    } else{
+        barraCor.style.display = 'none';
+    }
+
+    let btnCiano = document.querySelector('.ciano');
+    btnCiano.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--ciano)';
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--ciano)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--ciano)'; element.style.boxShadow = '0 0 4px var(--ciano)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--ciano)'; }); 
+    });
+    let btnAzulEscuro = document.querySelector('.azul-escuro');
+    btnAzulEscuro.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--azul-escuro)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--azul-escuro)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--azul-escuro)'; element.style.boxShadow = '0 0 4px var(--azul-escuro)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--azul-escuro)'; });
+    });
+    let btnRoxo = document.querySelector('.roxo');
+    btnRoxo.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--roxo)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--roxo)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--roxo)'; element.style.boxShadow = '0 0 4px var(--roxo)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--roxo)'; });
+    });
+    let btnRosaEscuro = document.querySelector('.rosa-escuro');
+    btnRosaEscuro.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--rosa-escuro)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--rosa-escuro)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa-escuro)'; element.style.boxShadow = '0 0 4px var(--rosa-escuro)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa-escuro)'; });
+    });
+    let btnRosa = document.querySelector('.rosa');
+    btnRosa.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--rosa)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--rosa)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa)'; element.style.boxShadow = '0 0 4px var(--rosa)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa)'; });
+    });
+    let btnVermelho = document.querySelector('.vermelho');
+    btnVermelho.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--vermelho)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--vermelho)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--vermelho)'; element.style.boxShadow = '0 0 4px var(--vermelho)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--vermelho)'; });
+    });
+    let btnAmarelo = document.querySelector('.amarelo');
+    btnAmarelo.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--amarelo)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--amarelo)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--amarelo)'; element.style.boxShadow = '0 0 4px var(--amarelo)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--amarelo)'; });
+    });
+    let btnVerde = document.querySelector('.verde');
+    btnVerde.addEventListener('click', () => {
+        document.querySelector('header').style.backgroundColor = 'var(--verde)'; 
+        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--verde)'; });
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--verde)'; element.style.boxShadow = '0 0 4px var(--verde)';});
+        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--verde)'; });
+    });
 }
