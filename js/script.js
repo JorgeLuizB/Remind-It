9b0d40db250b29e9495fc553e432ff9ab9de5dd9
JorgeLuiz <jrgluiz4848@gmail.com>
2023-01-30 22:44:18 -0300

    adicionado sistema de cores e corrigido um bug

--- js/script.js
+++ js/script.js
@@ -1,107 +1,105 @@
-let btnAdd = document.querySelector('.add');
-let btnClear = document.querySelector('.remove');
-const elemento = document.querySelector('.zona-add');
-btnAdd.addEventListener('click', () => {exibeFuncaoAddNota()})
-btnClear.addEventListener('click', () => {
-    window.localStorage.clear();
-    let espacoNota = document.querySelector('.principal');
+let elemento = document.querySelector('.zona-add');
+let espacoNota = document.querySelector('.principal');
+let barraCor = document.querySelector('.barra-lateral-suspensa');
+let mudouTema = false;
+let clicouBtnCor = false;
+
+function limpaNotas(){
+    localStorage.removeItem('notas');
     espacoNota.innerHTML = '';
-})
-let corID;
-let listaTitulos = [];
-let listaDescricoes = [];
-let listaCores = [];
-function exibeFuncaoAddNota(){
+}
+function fadeIn(){
     elemento.style.display = 'block';
     elemento.classList.add('anim-in');
-    selecionaCor();
 }
-function selecionaCor(){
-    let cores = document.getElementsByClassName('box');
-    for(let i = 0; i < cores.length; i++){
-        cores[i].addEventListener('click',() => {
-            corID = i;
-        });   
-    }
-}
-function adicionaNota(){
-    let espacoNota = document.querySelector('.principal');
-    let titulo = document.getElementById('txt-title');
-    let descricao = document.getElementById('txt-desc');
-    let titulos = '';
-    let descricoes = '';
-    let cores = '';
-    if(window.localStorage.key('titulo')){
-        titulos = window.localStorage.getItem('titulo');
-        listaTitulos = titulos.split(',');
-
-        descricoes = window.localStorage.getItem('descricao');
-        listaDescricoes = descricoes.split(',');
-
-        cores = window.localStorage.getItem('cor');
-        listaCores = cores.split(',');
-    }
-
-    if(titulo.value == ''){
-        titulo.focus();
-    } else if(descricao.value == ''){
-        descricao.focus();
-    } else{
-        /*let nota = `<div class="note"><div class="head"><p>${titulo.value}</p></div><div class="body"><div class="body-conteudo"><textarea readonly>${descricao.value}</textarea></div></div></div>`;*/
-
-        let nota = `<div class="note"><div class="head"><p>${titulo.value}</p></div><div class="body"><div class="body-conteudo"><div class="nota-info"><div class="ck-finalizado"><input type="checkbox" name="finalizado" id="finalizado"><label for="finalizado">Finalizado</label></div><div class="nota-data"><span>00/00/0000</span></div></div><p>${descricao.value}</p></div></div></div>`;
-        espacoNota.innerHTML += nota;
-        listaTitulos.push(titulo.value);
-        listaDescricoes.push(descricao.value)
-        listaCores.push(corID);
-        window.localStorage.setItem('titulo', listaTitulos);
-        window.localStorage.setItem('descricao', listaDescricoes);
-        window.localStorage.setItem('cor', listaCores);
-        titulo.value = '';
-        descricao.value = '';
-        escondeFuncaoAddNota();
-    }
-}
-
-function escondeFuncaoAddNota(){
+function fadeOut(){
     setTimeout(() => {elemento.style.display = 'none';}, 250);
     elemento.classList.remove('anim-in');
     elemento.classList.add('anim-out');
     setTimeout(() => {elemento.classList.remove('anim-out');}, 300);
 
 }
-function carregaNotasSalvas(){
-    let titulos = '';
-    let descricoes = '';
-    let cores = '';
-    let btnTema = document.querySelector('.theme');
-    let espacoNota = document.querySelector('.principal');
 
-    if(window.localStorage.key('titulo')){
-        titulos = window.localStorage.getItem('titulo').split(',');
-        descricoes = window.localStorage.getItem('descricao').split(',');
-        cores = window.localStorage.getItem('cor').split(',');
-        for(let i = 0; i < titulos.length; i++){
-            let nota = `<div class="note"><div class="head"><p>${titulos[i]}</p></div><div class="body"><div class="body-conteudo"><div class="nota-info"> <div class="ck-finalizado"><input type="checkbox" name="finalizado" id="finalizado"><label for="finalizado">Finalizado</label></div><div class="nota-data"><span>00/00/0000</span></div></div><p>${descricoes[i]}</p></div></div></div>`;
+function adicionaNota(){
+    let titulo = document.getElementById('txt-title');
+    let descricao = document.getElementById('txt-desc');
+
+    let notas = [];
+
+    if(titulo.value == ''){
+        titulo.focus();
+    } else if(descricao.value == ''){
+        descricao.focus();
+    } else{
+
+        if(localStorage.hasOwnProperty('notas')){
+            notas = JSON.parse(localStorage.getItem('notas'));
+            
+        }
+        //falta implementar data
+        let dataTemporaria = new Date();
+        let data = dataTemporaria.getDate() < 10 ? '0' + dataTemporaria.getDay() : dataTemporaria.getDate();
+        data += (dataTemporaria.getMonth() + 1) < 10 ? '0' + (dataTemporaria.getMonth() + 1) : dataTemporaria.getMonth() + 1;
+        data += dataTemporaria.getFullYear().toString();
+
+        notas.push({'titulo': titulo.value, 'descricao': descricao.value});
+        localStorage.setItem('notas', JSON.stringify(notas));
+        
+        let nota = `<div class="note"><div class="head"><p>${titulo.value}</p></div><div class="body"><div class="body-conteudo"><div class="nota-info"><div class="ck-finalizado"><input type="checkbox" name="finalizado" id="finalizado"><label for="finalizado">Finalizado</label></div><div class="nota-data"><span>00/00/0000</span></div></div><p>${descricao.value}</p></div></div></div>`;
+
+        espacoNota.innerHTML += nota;
+        if(localStorage.hasOwnProperty('cor')){
+            let cor = localStorage.getItem('cor');
+            document.querySelector('header').style.backgroundColor = `var(--${cor})`;
+            document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = `var(--${cor})`; });
+            document.querySelectorAll('.note').forEach(element => {element.style.borderColor = `var(--${cor})`; element.style.boxShadow = `0 0 4px var(--${cor})`;});
+            document.querySelectorAll('.note').forEach(element => {element.style.borderColor = `var(--${cor})`; }); 
+        } else{
+            ativaCiano();
+        }
+        titulo.value = '';
+        descricao.value = '';
+        fadeOut();
+    }
+
+}
+function carregaNotas(){
+    let btnTema = document.querySelector('.theme');
+
+    if(localStorage.hasOwnProperty('notas')){
+        let notas = []
+        notas = JSON.parse(localStorage.getItem('notas'));
+        for(let i = 0; i < notas.length; i++){
+            let nota = `<div class="note"><div class="head"><p>${notas[i]['titulo']}</p></div><div class="body"><div class="body-conteudo"><div class="nota-info"> <div class="ck-finalizado"><input type="checkbox" name="finalizado" id="finalizado"><label for="finalizado">Finalizado</label></div><div class="nota-data"><span>00/00/0000</span></div></div><p>${notas[i]['descricao']}</p></div></div></div>`;
+
             espacoNota.innerHTML += nota;
         }
     }
 
-    if(window.localStorage.key('Tema-modo')){
+    if(localStorage.hasOwnProperty('Tema-modo')){
         document.body.classList = window.localStorage.getItem('Tema-modo');
         btnTema.style.background = window.localStorage.getItem('Tema-img');
         btnTema.style.backgroundPosition = 'center';
 
+    } else{
+        btnTema.style.background = '#000000 url(../images/icon_dark.svg)';
+        btnTema.style.backgroundPosition = 'center';
     }
-
+    if(localStorage.hasOwnProperty('cor')){
+        let cor = localStorage.getItem('cor');
+        document.querySelector('header').style.backgroundColor = `var(--${cor})`;
+        document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = `var(--${cor})`; });
+        document.querySelectorAll('.note').forEach(element => {element.style.borderColor = `var(--${cor})`; element.style.boxShadow = `0 0 4px var(--${cor})`;});
+        document.querySelectorAll('.note').forEach(element => {element.style.borderColor = `var(--${cor})`; }); 
+    } else{
+        ativaCiano();
+    }
 }
-carregaNotasSalvas();
 
-let btnTema = document.querySelector('.theme');
-let mudouTema = false;
+function mudaTema() {
+    let btnTema = document.querySelector('.theme');
 
-btnTema.addEventListener('click', () => {
-    if(window.localStorage.key('Tema-mudouTema')){
+    if(localStorage.hasOwnProperty('Tema-mudouTema')){
         mudouTema = 'true' == window.localStorage.getItem('Tema-mudouTema') ? true : false;
     }
     mudouTema = !mudouTema;
@@ -109,31 +107,24 @@ btnTema.addEventListener('click', () => {
 
     if(mudouTema == true){
         btnTema.style.background = '#FFFFFF url(../images/icon_light.svg)';
-        window.localStorage.setItem('Tema-modo', 'light-theme dark-theme');
-        window.localStorage.setItem('Tema-img', '#FFFFFF url(../images/icon_light.svg)');
-        window.localStorage.setItem('Tema-mudouTema', mudouTema);
+        localStorage.setItem('Tema-modo', 'light-theme dark-theme');
+        localStorage.setItem('Tema-img', '#FFFFFF url(../images/icon_light.svg)');
+        localStorage.setItem('Tema-mudouTema', mudouTema);
         
     } else{
         
         btnTema.style.background = '#000000 url(../images/icon_dark.svg)';
-        window.localStorage.setItem('Tema-modo', 'light-theme');
-        window.localStorage.setItem('Tema-img','#000000 url(../images/icon_dark.svg)');
-        window.localStorage.setItem('Tema-mudouTema', mudouTema);
+        localStorage.setItem('Tema-modo', 'light-theme');
+        localStorage.setItem('Tema-img','#000000 url(../images/icon_dark.svg)');
+        localStorage.setItem('Tema-mudouTema', mudouTema);
         
     }
     btnTema.style.backgroundPosition = 'center';
 
+}
 
-    
-})
-
-let btnCor = document.querySelector('.change');
-btnCor.addEventListener('click', mudaCor);
-let clicouBtnCor = false;
-
-function mudaCor(){
+function ativaSelecaoCor(){
     clicouBtnCor = !clicouBtnCor;
-    let barraCor = document.querySelector('.barra-lateral-suspensa');
 
     if(clicouBtnCor){
         barraCor.style.display = 'flex';
@@ -141,60 +132,60 @@ function mudaCor(){
         barraCor.style.display = 'none';
     }
 
-    let btnCiano = document.querySelector('.ciano');
-    btnCiano.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--ciano)';
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--ciano)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--ciano)'; element.style.boxShadow = '0 0 4px var(--ciano)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--ciano)'; }); 
-    });
-    let btnAzulEscuro = document.querySelector('.azul-escuro');
-    btnAzulEscuro.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--azul-escuro)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--azul-escuro)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--azul-escuro)'; element.style.boxShadow = '0 0 4px var(--azul-escuro)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--azul-escuro)'; });
-    });
-    let btnRoxo = document.querySelector('.roxo');
-    btnRoxo.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--roxo)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--roxo)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--roxo)'; element.style.boxShadow = '0 0 4px var(--roxo)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--roxo)'; });
-    });
-    let btnRosaEscuro = document.querySelector('.rosa-escuro');
-    btnRosaEscuro.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--rosa-escuro)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--rosa-escuro)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa-escuro)'; element.style.boxShadow = '0 0 4px var(--rosa-escuro)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa-escuro)'; });
-    });
-    let btnRosa = document.querySelector('.rosa');
-    btnRosa.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--rosa)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--rosa)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa)'; element.style.boxShadow = '0 0 4px var(--rosa)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--rosa)'; });
-    });
-    let btnVermelho = document.querySelector('.vermelho');
-    btnVermelho.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--vermelho)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--vermelho)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--vermelho)'; element.style.boxShadow = '0 0 4px var(--vermelho)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--vermelho)'; });
-    });
-    let btnAmarelo = document.querySelector('.amarelo');
-    btnAmarelo.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--amarelo)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--amarelo)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--amarelo)'; element.style.boxShadow = '0 0 4px var(--amarelo)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--amarelo)'; });
-    });
-    let btnVerde = document.querySelector('.verde');
-    btnVerde.addEventListener('click', () => {
-        document.querySelector('header').style.backgroundColor = 'var(--verde)'; 
-        document.querySelectorAll('.head').forEach(element =>{element.style.backgroundColor = 'var(--verde)'; });
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--verde)'; element.style.boxShadow = '0 0 4px var(--verde)';});
-        document.querySelectorAll('.note').forEach(element =>{element.style.borderColor = 'var(--verde)'; });
-    });
 }
+function ativaCiano() {
+    localStorage.setItem('cor','ciano');
+    document.querySelector('header').style.backgroundColor = 'var(--ciano)';
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--ciano)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--ciano)'; element.style.boxShadow = '0 0 4px var(--ciano)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--ciano)'; }); 
+};
+function ativaAzulEscuro() {
+    localStorage.setItem('cor','azul-escuro');
+    document.querySelector('header').style.backgroundColor = 'var(--azul-escuro)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--azul-escuro)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--azul-escuro)'; element.style.boxShadow = '0 0 4px var(--azul-escuro)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--azul-escuro)'; });
+};
+function ativaRoxo() {
+    localStorage.setItem('cor','roxo');
+    document.querySelector('header').style.backgroundColor = 'var(--roxo)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--roxo)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--roxo)'; element.style.boxShadow = '0 0 4px var(--roxo)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--roxo)'; });
+};
+function ativaRosaEscuro() {
+    localStorage.setItem('cor','rosa-escuro');
+    document.querySelector('header').style.backgroundColor = 'var(--rosa-escuro)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--rosa-escuro)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--rosa-escuro)'; element.style.boxShadow = '0 0 4px var(--rosa-escuro)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--rosa-escuro)'; });
+};
+function ativaRosa() {
+    localStorage.setItem('cor','rosa');
+    document.querySelector('header').style.backgroundColor = 'var(--rosa)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--rosa)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--rosa)'; element.style.boxShadow = '0 0 4px var(--rosa)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--rosa)'; });
+};
+function ativaVermelho() {
+    localStorage.setItem('cor','vermelho');
+    document.querySelector('header').style.backgroundColor = 'var(--vermelho)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--vermelho)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--vermelho)'; element.style.boxShadow = '0 0 4px var(--vermelho)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--vermelho)'; });
+};
+function ativaAmarelo() {
+    localStorage.setItem('cor','amarelo');
+    document.querySelector('header').style.backgroundColor = 'var(--amarelo)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--amarelo)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--amarelo)'; element.style.boxShadow = '0 0 4px var(--amarelo)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--amarelo)'; });
+};
+function ativaVerde() {
+    localStorage.setItem('cor','verde');
+    document.querySelector('header').style.backgroundColor = 'var(--verde)'; 
+    document.querySelectorAll('.head').forEach(element => {element.style.backgroundColor = 'var(--verde)'; });
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--verde)'; element.style.boxShadow = '0 0 4px var(--verde)';});
+    document.querySelectorAll('.note').forEach(element => {element.style.borderColor = 'var(--verde)'; });
+};
